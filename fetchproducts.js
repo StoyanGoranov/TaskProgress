08bde240c8ea7590820c938fe716b6e9270a1f5b
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2020-08-31 17:06:44 +0300

    add event

--- fetchproducts.js
+++ fetchproducts.js
@@ -14,30 +14,16 @@ fetch('https://gist.githubusercontent.com/Raisolution/2fa773f5b7009d4b7c487e7a0a
 	//get data
 	.then(data => {
 		dataArr = data;
-		//console.log(data)
+		passDataEvent(dataArr);
 	})
-	
 	.catch(error => console.log(error))
 
-setTimeout(	() => {
-	console.log("dataObj initialised")
-	//since link-data is not valid JSON
-	//I decided to use eval() to be able to dynamically parse it
-	// although it's considered a huge security issue 
-	dataArr = eval(dataArr); 
-	console.log(dataArr);
-}, 500);
+//create custom event to pass data
+function passDataEvent(){
+	let event = new CustomEvent("passingData",  {
+		detail : eval(dataArr)
+	});
+	window.dispatchEvent(event);
+}
 
-// async function getData (url = ""){
-// 	const response = await fetch(url)
-// 		if(response.ok){
-// 			console.log("Successful fetch!")
-// 		} else {
-// 			console.log("Unsuccessful fetch!");
-// 		}
 
-// 		return response.json();
-// 	}
-
-// let data = getData('https://gist.githubusercontent.com/Raisolution/2fa773f5b7009d4b7c487e7a0ae553c2/raw/970973ec20df902b56078623d97dc0af92e388aa/gistfile1.txt')
-// console.log(data)
